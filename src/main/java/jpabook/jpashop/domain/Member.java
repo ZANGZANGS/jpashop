7694a67558ee0f3aaaa467b5b6e680c9787f7d44
zangzangs <kimjangh94@gmail.com>
2023-07-17 23:58:15 +0900

    .

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "zipcode")
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Orders> orders = new ArrayList<>();
+
+}
